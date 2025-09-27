0cb543583825b84fdd168e1455ca808cf7b222c4
website <sacinrawat6264384464@gmail.com>
2025-09-27 18:13:39 +0530

    connect databasein setting.py

--- CoachHub/settings.py
+++ CoachHub/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'CoachHub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',       
+        'USER': 'myuser',     
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
